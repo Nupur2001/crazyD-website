aab688a16ebcb4df3bfc0d95705189f8a458a49d
Nupur Sikka <67430495+Nupur2001@users.noreply.github.com>
2024-04-11 15:58:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// alert("Good")
+
+
 let menu = document.querySelector('#menu-bar');
 let navbar = document.querySelector('.navbar');
 
@@ -10,3 +13,34 @@ window.addEventListener('scroll', () => {
     menu.classList.remove('fa-times');
     navbar.classList.remove('active');
 });
+
+
+let countDate = new Date('April 13, 2024 00:00:00').getTime();
+
+function CountDown() {
+    let now = new Date().getTime();
+    let gap = countDate - now;
+
+    let second = 1000;
+    let minute = second * 60;
+    let hour = minute * 60;
+    let day = hour * 24;
+
+    let d = Math.floor(gap / day);
+    let h = Math.floor((gap % day) / hour);
+    let m = Math.floor((gap % hour) / minute);
+    let s = Math.floor((gap % minute) / second);
+
+    document.getElementById('day1').innerText = d;
+    document.getElementById('hour').innerText = h;
+    document.getElementById('minute').innerText = m;
+    document.getElementById('second').innerText = s;
+}
+
+// Initial Call
+CountDown();
+
+// Set Interval
+setInterval(function () {
+    CountDown();
+}, 1000);
